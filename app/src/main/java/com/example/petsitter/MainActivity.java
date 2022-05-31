6bef886e9563ac45f32a29f3108cdbc14ed2e00c
Miguel Tavares <miguel.tavares07@ua.pt>
2022-05-31 16:13:26 +0100

    app/src/main/java/com/example/petsitter/MainActivity.java app/src/main/java/com/example/petsitter/SignInActivity.java app/src/main/res/layout/signin.xml app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/petsitter/MainActivity.java
+++ app/src/main/java/com/example/petsitter/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
                 startActivity(intent);
             }
-        },5000);
+        },2000);
 
 
     }
