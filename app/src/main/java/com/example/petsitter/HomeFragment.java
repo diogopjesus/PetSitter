234896c2bc8b626754f978d048255c0642acefa1
Diogo Jesus <73116644+diogopjesus@users.noreply.github.com>
2022-06-04 04:48:25 +0100

    Add TabLayout to Home Page. TODO:Verify if loading works as intended dynamically. NOTE: Static loading works just fine.

--- app/src/main/java/com/example/petsitter/HomeFragment.java
+++ app/src/main/java/com/example/petsitter/HomeFragment.java
@@ -1,16 +1,23 @@
 package com.example.petsitter;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.petsitter.databinding.FragmentHomeBinding;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link MenuFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
@@ -28,17 +35,24 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private TabLayout _homeTabLayout;
+    private ViewPager _homeViewPager;
+
+    // tab titles
+    private String[] titles = new String[]{"Ongoing", "Upcoming", "Past"};
+    FragmentHomeBinding binding;
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
+     * @return A new instance of fragment MenuFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
+    public static MenuFragment newInstance(String param1, String param2) {
+        MenuFragment fragment = new MenuFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +73,49 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        inflater.inflate(R.layout.fragment_home, container, false);
+
+        binding = FragmentHomeBinding.inflate(getLayoutInflater());
+        View v = binding.getRoot();
+        init();
+
+        return v;
+    }
+
+    private void init() {
+        // removing toolbar elevation
+        MainActivity activity = (MainActivity)getActivity();
+
+        binding.viewPager.setAdapter(new ViewPagerFragmentAdapter(activity));
+
+        // attaching tab mediator
+        new TabLayoutMediator(binding.tabLayout, binding.viewPager,
+                (tab, position) -> tab.setText(titles[position])).attach();
+    }
+
+    private class ViewPagerFragmentAdapter extends FragmentStateAdapter {
+
+        public ViewPagerFragmentAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0:
+                    return new OngoingFragment();
+                case 1:
+                    return new UpcomingFragment();
+                case 2:
+                    return new PastFragment();
+            }
+            return new OngoingFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return titles.length;
+        }
     }
 }
